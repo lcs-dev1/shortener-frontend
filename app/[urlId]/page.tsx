79dd3b25fc10a1119991d26e8c0faed2830c4f73
Lucas M <contato.lucasm.dev@gmail.com>
2024-02-29 03:50:58 -0300

    removing styled-components and some tweaks
    feat: added new regex for field validation
    feat: removed unnecessary .env key
    feat: removed styled-components

--- app/[urlId]/page.tsx
+++ app/[urlId]/page.tsx
@@ -1,7 +1,6 @@
 import { getLink } from "./fetch";
 import { redirect } from "next/navigation";
-import * as S from "./styles";
-
+import styles from "./styles.module.scss"
 export default async function Page({ params }: { params: { urlId: string } }) {
   const link = await getLink(params.urlId);
   if (link.fullUrl) {
@@ -9,19 +8,19 @@ export default async function Page({ params }: { params: { urlId: string } }) {
   }
   if (link.code === 404) {
     return (
-      <S.ContainerError>
+      <div className={styles.containerError}>
         <h2>OOOOPS!</h2>
         <h1>404</h1>
         <p>We didn&apos;t find the page you&apos;re looking for!</p>
-      </S.ContainerError>
+      </div>
     );
   }
 
   return (
-    <S.ContainerError>
+    <div className={styles.containerError}>
       <h2>Something went wrong...</h2>
       <h1>{link.code}</h1>
       <p>Sorry about that!</p>
-    </S.ContainerError>
+    </div>
   );
 }
